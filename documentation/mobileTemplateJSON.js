b53f0bc73b38cf6829e84fd9606693e50a2d83fc
Corneliu <corneliu.gaina@gmail.com>
2019-05-20 16:57:39 +0200

    template json mobile

---
+++ documentation/mobileTemplateJSON.js
@@ -0,0 +1,30 @@
+games : [ 
+    { 
+        id : , 
+        name : , 
+        users : [ 
+            { 
+                id : ,
+                name : ,
+                scoreTotal: , 
+                answers : [ 
+                    { 
+                        id:
+                        response: true/false
+                        timing : 
+                    }, 
+                    {
+                        id:
+                        response: true/false
+                        timing : 
+                    },
+                    {
+                        id:
+                        response: true/false
+                        timing : 
+                    }  
+                ]
+            }
+        ]
+    }
+]
