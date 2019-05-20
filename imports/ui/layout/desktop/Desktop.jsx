d2a45c01f6e2be2145cfaba35d34ea8831ce8ac0
Pierre-Louis Picard <Pierre-Louis@Ioanas-MacBook-Air.local>
2019-05-20 14:23:14 +0200

    route

--- imports/ui/layout/desktop/Desktop.jsx
+++ imports/ui/layout/desktop/Desktop.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React from 'react';
 import DesktopContainer from '../../container/desktop/MainContainer/MainContainer'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+ 
 
-const Desktop = () => <DesktopContainer />
+const Desktop = () => (
+    <Router>
+        <DesktopContainer />
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/room">Room</Link>
+                </li>
+            </ul>
+            <hr />
+            
+            <Route exact path="/" component={Home} />
+            <Route path="/room" component={Room} />
+            
+        </div>
+    </Router>
+);
 
-export default Desktop
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+function Room() {
+    return (
+        <div>
+            <h2>Room</h2>
+        </div>
+    );
+}
+
+export default Desktop;
