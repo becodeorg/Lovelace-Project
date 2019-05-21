0b719eee0084629c93619089305c984b25f1bda4
Corneliu <corneliu.gaina@gmail.com>
2019-05-21 14:50:00 +0200

    Feature/podium : Podium component, importing Users

--- imports/ui/container/mobile/Podium/Podium.jsx
+++ imports/ui/container/mobile/Podium/Podium.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Users } from '../../../components/Salon/Users'
 
 export default class Podium extends Component {
-    
     render() {
         return (
             <div>
