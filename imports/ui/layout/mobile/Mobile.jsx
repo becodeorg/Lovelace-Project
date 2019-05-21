8e2a2ed1bb2abafcb3a0316e659dc6e89ab6c5aa
Corneliu <corneliu.gaina@gmail.com>
2019-05-21 14:33:56 +0200

    Feature/podium : Podium component, importing Users

--- imports/ui/layout/mobile/Mobile.jsx
+++ imports/ui/layout/mobile/Mobile.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import MobileContainer from '../../container/mobile/MainContainer/MainContainer'
+import MainContainer from '../../container/mobile/MainContainer/MainContainer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import RoomPlayers from '../../container/mobile/HomeMobile/RoomPlayers'
 import Question from '../../container/mobile/MobileQuestion/Question'
@@ -8,12 +8,11 @@ import Podium from '../../container/mobile/Podium/Podium'
 
 const Mobile = () => (
     <Router basename={'admin'}>
-        <MobileContainer />
+        <MainContainer/>
         {/* <Route exact path="/" component={LandingPage} /> */}
         <Route path="/roomplayers" component={RoomPlayers} />
         <Route path="/question/:question_id" component={Question} />
         <Route path="/podium" component={Podium} />
     </Router>
-
-) 
+)
 export default Mobile
