3e8c7e19dc80bdc0110338b2f75f88a56229ece2
Elisa Renda <elisa.renda@hotmail.fr>
2019-05-21 11:14:05 +0200

    RoomPlayers

---
+++ imports/ui/components/Mobile/HomeMobile/RoomPlayers.jsx
@@ -0,0 +1,24 @@
+const room =
+    {
+        id: '123456', // salon
+        name: 'react', // nom de la partie
+        current: '1234567890',
+        displayAnswers: false,
+        usersjoined: ['1234567'],
+        question: [
+            {
+                id: '1234567890',
+                displayedAt: 123456789,
+                title: 'ceci est un titre',
+                description: 'ceci est un titre',
+                answers: [
+                    {
+                        id: 'azertyhn,;vd;s,k',
+                        response: 1,
+                        answersAt: 1234567890765,
+                    },
+                ],
+            },
+        ],
+    },
+
