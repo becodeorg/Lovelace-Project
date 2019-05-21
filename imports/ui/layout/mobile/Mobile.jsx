20deefe8cf4841e27f8e0ed6eb58a6ee94c1f550
Emilie Bialais <ebialais@gmail.com>
2019-05-21 10:39:12 +0200

    modif route ajout ds /layout/mobile/Mobile.jsx

--- imports/ui/layout/mobile/Mobile.jsx
+++ imports/ui/layout/mobile/Mobile.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import MobileContainer from '../../container/mobile/MainContainer/MainContainer'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import RoomPlayers from '../components/Mobile/HomeMobile/RoomPlayers'
+import Question from '../components/Mobile/MobileQuestion/Question'
+import Podium from '../components/Mobile/Podium/Podium'
 
-const Mobile = () => <MobileContainer />
+const Mobile = () => (
+    <Router basename={'admin'}>
+        <MobileContainer />
+        <Route exact path="/" component={Home} />
+        <Route path="/roomplayers" component={RoomPlayers} />
+        <Route path="/question/:question_id" component={Question} />
+        <Route path="/podium" component={Podium} />
+    </Router>
 
+) 
 export default Mobile
